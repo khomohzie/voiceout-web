e354b60b511e1afae9da064da7788cd4a779551c
Daniel Komolafe <komozy2000@gmail.com>
2023-04-08 02:59:18 +0100

    setup global styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+      {
+        source: "/index",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
